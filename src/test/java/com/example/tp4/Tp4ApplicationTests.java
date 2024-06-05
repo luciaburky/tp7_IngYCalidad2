67743d3a63f9d3d891286fee481c3f3e96d35026
luciaburky <luciaburky1@gmail.com>
2024-06-05 11:27:57 -0300

    test de que existe clase Venta

--- src/test/java/com/example/tp4/Tp4ApplicationTests.java
+++ src/test/java/com/example/tp4/Tp4ApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.tp4;
 
+import com.example.tp4.entities.Venta;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class Tp4ApplicationTests {
@@ -9,5 +12,13 @@ class Tp4ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void test() {
+		Venta venta = new Venta();
+		assertNotNull(venta);
+	}
+
+
 
 }
---
+++ src/test/java/com/example/tp4/entities/Venta.java
@@ -0,0 +1,4 @@
+package com.example.tp4.entities;
+
+public class Venta {
+}
